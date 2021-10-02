6c6b8983cd2eaaaadd473505bd7828e4c665bafc
Kovlyn <kovlyn.reddy@gmail.com>
2021-10-02 14:08:35 +0200

    SendMainInit

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace InuranceAssignmentAPD03.Controllers
@@ -83,5 +88,87 @@ namespace InuranceAssignmentAPD03.Controllers
                 return View();
             }
         }
+
+        private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            // Get the unique identifier for this asynchronous operation.
+            String token = (string)e.UserState;
+
+            if (e.Cancelled)
+            {
+                Console.WriteLine("[{0}] Send canceled.", token);
+            }
+            if (e.Error != null)
+            {
+                Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Message sent.");
+            }
+
+        }
+
+
+        public async Task SendMail()
+        {
+ 
+            #region mail1
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.Credentials = new System.Net.NetworkCredential("Techno Solutions01", "T3$0em01");
+            client.Port = 587;
+            client.EnableSsl = true;
+            // Specify the email sender.
+            // Create a mailing address that includes a UTF8 character
+            // in the display name.
+            MailAddress from = new MailAddress("TechnoSolutions0001@gmail.com",
+               "Techno" + (char)0xD8 + "Solutions01", System.Text.Encoding.UTF8);
+            // Set destinations for the email message.
+            MailAddress to = new MailAddress("kovlyn.reddy@gmail.com");
+            // Specify the message content.
+
+            MailMessage message = new MailMessage(from, to);
+
+            message.Body = "Ypu have Purchased" ;
+            // Include some non-ASCII characters in body and subject.
+            string someArrows = new string(new char[] { '\u2190', '\u2191', '\u2192', '\u2193' });
+            message.Body += Environment.NewLine + someArrows;
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.Subject = "Thank you For your Purchase ! " + someArrows;
+            message.SubjectEncoding = System.Text.Encoding.UTF8;
+            // Set the method that is called back when the send operation ends.
+            client.SendCompleted += new SendCompletedEventHandler(SendCompletedCallback);
+            // The userState can be any object that allows your callback
+            // method to identify this send operation.
+            // For this example, the userToken is a string constant.
+
+
+            //client.Send(message);
+
+            #endregion
+
+            #region Mail2
+
+            var sender = new SmtpSender(() => new SmtpClient(host: "smtp.gmail.com")
+            {
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Port = 587,
+                Credentials = new NetworkCredential("TechnoSolutions0001@gmail.com", "T3$0em01")
+            });
+
+            Email.DefaultSender = sender;
+
+            var email = await Email
+                .From(emailAddress: "TechnoSolutions0001@gmail.com")
+                .To(emailAddress: "kovlyn.reddy@gmail.com", name: "Kovlyn Reddy")
+                .Subject(subject: "Thanks for the Purchase !")
+                .Body(body: "Items Purchase =")
+                .SendAsync();
+
+            #endregion
+        }
     }
 }
+
